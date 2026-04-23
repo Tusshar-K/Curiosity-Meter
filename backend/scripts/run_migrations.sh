b33ec625d0e88b89b462d9407e02ba7bc3fc6fb2
Tusshar Koppula <tushar.k0671@gmail.com>
2026-04-23 12:43:25 +0530

    fixed DB URL var name for migration 002

--- backend/scripts/run_migrations.sh
+++ backend/scripts/run_migrations.sh
@@ -4,7 +4,7 @@ set -e
 echo "=== Running database migrations ==="
 
 psql -d "$POSTGRES_DB_URL" -f migrations/001_scoring_and_topic_updates.sql
-psql -d "$POSTGRES_DB_URLL" -f migrations/002_vector_store_and_topic_map.sql
+psql -d "$POSTGRES_DB_URL" -f migrations/002_vector_store_and_topic_map.sql
 psql -d "$POSTGRES_DB_URL" -f migrations/003_session_question_budget.sql
 psql -d "$POSTGRES_DB_URL" -f migrations/004_questions_post_nudge.sql
 psql -d "$POSTGRES_DB_URL" -f migrations/005_give_up_events.sql
